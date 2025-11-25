1fe59706d03e9436971b898c3f953d21990c76c5
杨再涛 <1772081518@qq.com>
2025-11-26 00:28:53 +0800

    绑定app数据

--- Project/WpfApp/MainWindow.xaml.cs
+++ Project/WpfApp/MainWindow.xaml.cs
@@ -16,14 +16,36 @@ using System.Windows.Shapes;
 
 namespace WpfApp
 {
+    public class AppInfo
+    {
+        public string Title { get; set; }
+        public string Color_Text_0 { get; set; }
+        public string Color_0 { get; set; }
+        public string Color_1 { get; set; }
+        public string Color_2 { get; set; }
+
+        // 构造函数初始化数据
+        public AppInfo()
+        {
+            Title = "JDK管理";
+            Color_Text_0 = "#E2852E";
+            Color_0 = "#E2852E";
+            Color_1 = "#F5C857";
+            Color_2 = "#FFEE91";
+        }
+    }
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string Title_ { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            var app = new AppInfo();
+            DataContext = app;
+            Title = app.Title;
         }
 
 
@@ -46,7 +68,15 @@ namespace WpfApp
         }
         private void SymbolIcon_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Close();
+           var info =  MessageBox.Show(
+                "确定退出程序？",
+                "提示",
+                MessageBoxButton.OKCancel,
+                MessageBoxImage.Question
+           );
+            if (info == MessageBoxResult.OK) {
+                Close();
+            }
         }
     }
 }
